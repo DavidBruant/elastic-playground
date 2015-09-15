2e762e4383d57b608fc987d56c0cda972a03a35c
David Bruant <bruant.d@gmail.com>
2015-09-15 22:18:34 +0200

    bigrams and more-grams work

--- problem2.js
+++ problem2.js
@@ -4,6 +4,8 @@ require('es6-shim');
 
 var INDEX_NAME = "espg";
 var TYPE_NAME = "espg_doc";
+var FILLER_TOKEN = '_';
+
 
 var prepareIndex = require('./src/prepareIndex');
 
@@ -15,10 +17,6 @@ var settings = {
                     "type": "stop",
                     "stopwords": "_english_"
                 },
-                /*"english_keywords": {
-                    "type": "keyword_marker",
-                    "keywords": []
-                },*/
                 "english_stemmer": {
                     "type": "stemmer",
                     "language": "english"
@@ -27,22 +25,40 @@ var settings = {
                     "type": "stemmer",
                     "language": "possessive_english"
                 },
-                shingle: {
+                shingle_bi: {
                     type: "shingle",
+                    min_shingle_size: 2,
+                    max_shingle_size: 2,
+                    output_unigrams: false,
+                    filler_token: FILLER_TOKEN
+                },
+                shingle_more: {
+                    type: "shingle",
+                    min_shingle_size: 3,
                     max_shingle_size: 5,
-                    output_unigrams: false
+                    output_unigrams: false,
+                    filler_token: FILLER_TOKEN
                 }
             },
             "analyzer": {
-                "english_shingles": {
+                "english_shingles_bi": {
                     "tokenizer": "standard",
                     "filter": [
                         "english_possessive_stemmer",
                         "lowercase",
                         "english_stop",
-                        //"english_keywords",
                         "english_stemmer",
-                        "shingle"
+                        "shingle_bi"
+                      ]
+                },
+                "english_shingles_more": {
+                    "tokenizer": "standard",
+                    "filter": [
+                        "english_possessive_stemmer",
+                        "lowercase",
+                        "english_stop", // don't remove stop words for 3-4-5-grams
+                        "english_stemmer",
+                        "shingle_more"
                       ]
                 }
             }
@@ -54,7 +70,16 @@ var settings = {
             properties: {
                 "text": {
                     "type": "string",
-                    "analyzer": "english_shingles"
+                    "fields": {
+                        bi: {
+                            type: 'string',
+                            analyzer: "english_shingles_bi"
+                        },
+                        more: {
+                            type: 'string',
+                            analyzer: "english_shingles_more"
+                        }
+                    }
                 }
             }
         }
@@ -65,39 +90,58 @@ var settings = {
 prepareIndex(INDEX_NAME, TYPE_NAME, settings)
     .then(function (client) {
         setTimeout(function tryAnalyze(){
+            console.time('espg');
+            
             return client.termvector({
                 index: INDEX_NAME,
                 type: TYPE_NAME,
-                id: 'web design.txt',
-                fields: 'text'
+                id: 'wikip-x-men.txt',
+                fields: ['text.bi', 'text.more']
             }).then(function (resp) {
                 console.log('resp', resp);
-                var esterms = resp.term_vectors.text.terms;
+                var estermsbi = resp.term_vectors['text.bi'].terms;
+                var estermsmore = resp.term_vectors['text.more'].terms;
                 
-                var terms = Object.keys(esterms);
-                var termsWithFreq = terms
+                var biterms = Object.keys(estermsbi);
+                var moreterms = Object.keys(estermsmore);
+                
+                var biTermsWithFreq = biterms
                     .filter(function(t){
-                        return !t.includes('_'); // filler_token
+                        return !t.includes(FILLER_TOKEN) && estermsbi[t].term_freq >= 2;
                     })
                     .map(function(t){
-                        var freq = esterms[t].term_freq;
+                        var freq = estermsbi[t].term_freq;
 
                         return {
                             word: t,
                             freq: freq
                         };
                     });
+                var moreTermsWithFreq = moreterms
+                    .filter(function(t){
+                        return !t.includes(FILLER_TOKEN) && estermsmore[t].term_freq >= 2;
+                    })
+                    .map(function(t){
+                        var freq = estermsmore[t].term_freq;
+
+                        return {
+                            word: t,
+                            freq: freq
+                        };
+                    });
+                
+                var termsWithFreq = [].concat(biTermsWithFreq).concat(moreTermsWithFreq);
 
                 termsWithFreq.sort(function(tf1, tf2){
                     return tf2.freq - tf1.freq;
                 });
                 // now they're sorted
 
-                var relevantTerms = termsWithFreq.slice(0, 20);
+                var importantTerms = termsWithFreq.slice(0, 30);
 
 
-                console.log('termsWithFreq', JSON.stringify(relevantTerms, null, 3));
-
+                console.log('termsWithFreq', JSON.stringify(importantTerms, null, 3));
+                console.timeEnd('espg');
 
             }, function (err) {
                 console.trace(err);
